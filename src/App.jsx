80a34da88dede64e65fd0efa2bd70ec10fcb1151
x7x <137330630+userx7c@users.noreply.github.com>
2025-11-19 03:43:11 +0100

    Refactor imports and maintain component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./sections/navbar";
+import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Projects from "./sections/Projects";
@@ -22,3 +22,4 @@ const App = () => {
 };
 
 export default App;
+
